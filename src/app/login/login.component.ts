c96794093b059446f3926ef4c261d6cf212b7fda
nidhal <nidhal.jelassi@fsegt.utm.tn>
2022-04-05 13:17:20 +0100

    add a login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  defaultSection = 'dmwm';
+  comment = 'Rien à dire...';
   constructor() {}
 
   ngOnInit(): void {}
 
   submitHandler(f) {
     console.log(f.value);
+  }
+
+  randomPwd(f: NgForm) {
+    f.setValue({
+      login: '',
+      mdp: 'radnompassword123',
+      section: '',
+      commentaire: '',
+    });
+  }
+
+  randomPwd2(f: NgForm) {
+    f.form.patchValue({
+      mdp: 'radnompassword123',
+    });
+  }
+
+  ResetForm(f: NgForm) {
+    f.reset();
   }
 }
